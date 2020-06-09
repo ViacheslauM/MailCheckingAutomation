39944826f3b83adc36198c033743f50d12f00aa7
ViacheslauM <viacheslau@mail.com>
2020-06-09 16:08:26 +0300

    Project Created

---
+++ EntryPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace MailLoginAutomation
+{
+    class MailLoginAutomation
+    {
+        static void Main(string[] args)
+        {
+            var login = new Login();
+            var newMailChecker = new NewMailChecker();
+            IWebDriver driver = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(130));
+
+            login.LogIn(driver, wait, "csharppractice", "FoundationOfProgramming");
+            Console.WriteLine(newMailChecker.GetNewMails(wait));
+
+        }
+    }
+}
---
+++ Login.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace MailLoginAutomation
+{
+    class Login
+    {
+        public void LogIn(IWebDriver driver, WebDriverWait wait, string name, string pass)
+        {
+            driver.Url = "https://account.mail.ru/";
+
+            IWebElement username = wait.Until(ExpectedConditions.ElementIsVisible(By.Name("username")));
+            username.SendKeys(name);
+            username.SendKeys(Keys.Return);
+
+            IWebElement password = wait.Until(ExpectedConditions.ElementIsVisible(By.Name("password")));
+            password.SendKeys(pass);
+            password.SendKeys(Keys.Return);
+        }
+    }
+}
